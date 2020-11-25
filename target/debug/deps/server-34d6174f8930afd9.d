ea044e797ef7acfed36fd45afe8e9130df939cb6
cowboy8625 <43012445+cowboy8625@users.noreply.github.com>
2020-11-25 14:43:05 -0600

    Made chat client and server a different project.

---
+++ target/debug/deps/server-34d6174f8930afd9.d
@@ -0,0 +1,5 @@
+c:\Users\Bobby\Documents\Programing\Rust\rusty-chat\chat\server\target\debug\deps\server-34d6174f8930afd9.rmeta: src\main.rs
+
+c:\Users\Bobby\Documents\Programing\Rust\rusty-chat\chat\server\target\debug\deps\server-34d6174f8930afd9.d: src\main.rs
+
+src\main.rs:
