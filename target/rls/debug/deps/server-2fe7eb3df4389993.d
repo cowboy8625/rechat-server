03317e20527625d38eece4e1849984aad1f95141
cowboy8625 <43012445+cowboy8625@users.noreply.github.com>
2020-11-25 15:04:44 -0600

    Added files not needed

--- target/rls/debug/deps/server-2fe7eb3df4389993.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Bobby\Documents\Programing\Rust\rechat\rechat-server\target\rls\debug\deps\server-2fe7eb3df4389993.rmeta: src\main.rs
-
-c:\Users\Bobby\Documents\Programing\Rust\rechat\rechat-server\target\rls\debug\deps\server-2fe7eb3df4389993.d: src\main.rs
-
-src\main.rs:
